4ab7cfcd8d07b1b4d17b935e464f1fc26f16a6ac
hermance <hermancedornier@gmail.com>
2019-04-27 13:22:12 +0200

    mise en place du dashboard

--- home.php
+++ home.php
@@ -1,8 +1,18 @@
-<div class="wrapper" id="wrapper-index">
+<div class="container">
     <h1><?php echo HELLO; ?></h1>
         <?php if(isset($_SESSION["firstname"])){
            ?><h2><?php echo $_SESSION["firstname"]; ?> </h2> <?php
         }
          ?>
      </h2>
+     <div class="dashboard-container">
+        <div class="row">
+            <div class="tile col">TILE 1</div>
+            <div class="tile col">TILE 2</div>
+        </div>
+        <div class="row">
+            <div class="tile col">TILE 3</div>
+            <div class="tile col">TILE 4</div>
+        </div>
+     <div>
 </div>
