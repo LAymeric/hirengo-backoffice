744481eec0c3210762da448e8f53f1c62563e9ee
hermance <hermancedornier@gmail.com>
2019-05-05 19:37:34 +0200

    design BO

--- driver/dashboardDriver.php
+++ driver/dashboardDriver.php
@@ -3,7 +3,6 @@
         <div class="row">
             <div class="tile col" onclick="window.location.href='./driver/availableCourses.php'"><i class="fas fa-list-ul big-icon"></i><?php echo AVAILABLE_COURSES; ?></div>
             <div class="tile col" onclick="window.location.href='./driver/courseHistory.php'"><i class="fas fa-history big-icon"></i><?php echo COURSES_HISTORY; ?></div>
-            <div class="tile col" onclick="window.location.href='./driver/yourServices.php'"><i class="fas fa-hand-holding-heart big-icon"></i><?php echo YOUR_SERVICES; ?></div>
         </div>
         <div class="row">
             <div class="tile col" onclick="window.location.href='./driver/myCar.php'"><i class="fas fa-car big-icon"></i><?php echo MY_CAR; ?></div>
